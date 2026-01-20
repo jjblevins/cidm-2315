072b0c1947f44e025dcc4710911da34eb1c084e5
jjblevins <jblevins1@buffs.wtamu.edu>
2026-01-20 14:14:36 -0600

    Add files via upload

---
+++ Homework 1.cs.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double variable_X = 2.5;
+        double variable_Y = 3.3;
+        double total_Z = (4 * variable_X * variable_X)+(3 * variable_Y);
+        Console.WriteLine($"The value of Z is {total_Z}");
+    }
+}
